bdc6c27e69486b6c4f4bcc0b3133d42619355058
JB <bradburyjesse7@gmail.com>
2024-04-01 21:29:58 -0700

    Adding Artists components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Artists from './pages/Artists';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import ErrorPage from './pages/ErrorPage';
@@ -24,6 +25,9 @@ const router = createBrowserRouter([
       }, {
         path: '/signup',
         element: <Signup />
+      }, {
+        path: '/artists',
+        element: <Artists />
       }, {
         path: '/profiles/:username',
         element: <Profile />
