e1cf092df951fe834f40b3a809cf47eb8fac1c7c
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-04-04 18:43:59 -0700

    single product page

--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -2,14 +2,15 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
-import { QUERY_PRODUCTS } from "../utils/queries";
+import { QUERY_SINGLE_PRODUCT } from "../utils/queries";
 
 const SingleThought = () => {
   // Use `useParams()` to retrieve value of the route parameter `:id`
   const { id } = useParams();
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS, {
+  const { loading, data } = useQuery(QUERY_SINGLE_PRODUCT, {
     // pass URL parameter
+    variables: { id: id },
   });
 
   const product = data?.product || {};
@@ -18,11 +19,19 @@ const SingleThought = () => {
     return <div>Loading...</div>;
   }
 
-  if (data) {
-    console.log(data);
-    // console.log(products);
-  }
-  return <div className="my-3">Test product</div>;
+  console.log(product);
+
+  return (
+    <div className="my-3">
+      <h2>{product._id}</h2>
+      <h2>{product.name}</h2>
+      <h2>{product.imageUrl}</h2>
+      <h2>{product.artistName}</h2>
+      <h2>{product.imageUrl}</h2>
+      <h2>{product.isActive}</h2>
+      <h2>${product.price}</h2>
+    </div>
+  );
 };
 
 export default SingleThought;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -46,6 +46,8 @@ export const QUERY_SINGLE_PRODUCT = gql`
       isActive
       price
       productName
+      artistName
+      imageUrl
     }
   }
 `;
