46be4fcaf17e72166a3821efb26ba44664bf8c3b
victorgarrido1 <vcgarr94@gmail.com>
2024-04-04 20:56:18 -0700

    post jessie push?

--- client/src/pages/Artists.jsx
+++ client/src/pages/Artists.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client';
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 // This will pull the data from all the Artists. 
 import { QUERY_ARTISTS } from '../utils/queries';
