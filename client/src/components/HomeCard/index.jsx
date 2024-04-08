b35188717cc8e328382913ecddc2fa5eeb6c9abd
JB <bradburyjesse7@gmail.com>
2024-04-08 15:55:51 -0700

    Added homeCard component and set up links for home page

--- client/src/components/HomeCard/index.jsx
+++ client/src/components/HomeCard/index.jsx
@@ -1,18 +1,17 @@
 const HomeCard = ({ feature }) => {
     return (
-        <div className=" rounded-md shadow-lg w-60 mx-4 justify-center">
-            <img
-                src={feature.imageUrl}
-                alt={feature.title}
-                className="object-cover w-full h-40 rounded-t-md"
-            />
-            <div className="p-4">
-                <h3 className="text-lg font-semibold mb-2">{feature.title}</h3>
-
-                <p className="text-lg font-semibold">{feature.splash}</p>
-            </div>
+      <div className="relative h-64 overflow-hidden rounded-md mx-4">
+        <img
+          src={feature.imageUrl}
+          alt={feature.title}
+          className="w-full h-full object-cover rounded-md"
+        />
+        <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4">
+          <h3 className="text-lg font-semibold">{feature.title}</h3>
+          <p className="text-sm">{feature.splash}</p>
         </div>
+      </div>
     );
-};
+  };
 
 export default HomeCard;
