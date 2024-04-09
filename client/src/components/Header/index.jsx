dfa1a3b2f14978d18c50f32591a54ccc13ff0db3
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-04-08 20:14:56 -0700

    responsive header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -2,62 +2,182 @@ import { Link } from "react-router-dom";
 
 import Auth from "../../utils/auth";
 
+import { useState, useRef, useEffect } from "react";
+
+import { styled, useTheme } from "@mui/material/styles";
+
+import { Hidden } from "@mui/material";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
 const style = "hover:underline underline-offset-2";
 
+const drawerWidth = 240;
+
+const options = [
+  { name: "Shop", link: "/shop" },
+  { name: "Artists", link: "/artists" },
+  { name: "About the Fair", link: "/about" },
+  { name: "Favorites", link: "/favorites" },
+];
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.easeOut,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  ...(open && {
+    width: `calc(100%)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
 const Header = () => {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <header className="w-screen flex justify-center mb-8">
-      <div className="flex w-4/5 justify-between p-5 items-center border-2 border-black bg-transparent text-black">
-        <div>
-          <Link className="text-xl" to="/">
-            Art Fair
-          </Link>
+    <div>
+      <AppBar sx={{ bgcolor: "transparent" }} className="flex items-center" elevation={0} color="default" position="relative" open={open}>
+        <div className="flex w-screen lg:w-4/5 justify-between p-5 items-center border-2 border-black bg-transparent text-black">
+          <div className="cursor-pointer">
+            <Link className="text-xl" to="/">
+              Art Fair
+            </Link>
+          </div>
+          <Hidden lgUp>
+            <IconButton color="inherit" aria-label="open drawer" edge="end" onClick={handleDrawerOpen} sx={{ ...(open && { display: "none" }) }}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+
+          <Hidden lgDown>
+            <div className="flex gap-5">
+              {Auth.loggedIn() ? (
+                <>
+                  <Link className={style} to="/shop">
+                    Shop
+                  </Link>
+                  <Link className={style} to="/artists">
+                    Artists
+                  </Link>
+                  <Link className={style} to="/about">
+                    About the Fair
+                  </Link>
+                  <Link className={style} to="/favorites">
+                    Favorites
+                  </Link>
+                  <button className="text-red-600" onClick={logout}>
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link className={style} to="/shop">
+                    Shop
+                  </Link>
+                  <Link className={style} to="/artists">
+                    Artists
+                  </Link>
+                  <Link className={style} to="/about">
+                    About the Fair
+                  </Link>
+                  <Link className={style} to="/login">
+                    Login
+                  </Link>
+                  <Link className={style} to="/signup">
+                    Signup
+                  </Link>
+                </>
+              )}
+            </div>
+          </Hidden>
         </div>
-        <div className="flex gap-5">
-          {Auth.loggedIn() ? (
-            <>
-              <Link className={style} to="/shop">
-                Shop
-              </Link>
-              <Link className={style} to="/artists">
-                Artists
-              </Link>
-              <Link className={style} to="/about">
-                About the Fair
-              </Link>
-              <Link className={style} to="/favorites">
-                Favorites
-              </Link>
-              <button className="text-red-600" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className={style} to="/shop">
-                Shop
-              </Link>
-              <Link className={style} to="/artists">
-                Artists
-              </Link>
-              <Link className={style} to="/about">
-                About the Fair
-              </Link>
-              <Link className={style} to="/login">
-                Login
-              </Link>
-              <Link className={style} to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
+      </AppBar>
+
+      <Hidden lgUp>
+        <Drawer
+          ref={ref}
+          sx={{
+            width: open ? drawerWidth : 0,
+            // flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              background: "white",
+              color: "black",
+              border: "2px solid black",
+              width: open ? drawerWidth : 0,
+            },
+          }}
+          variant="temporary"
+          anchor="right"
+          open={open}
+          ModalProps={{ onBackdropClick: handleDrawerClose }}
+        >
+          <div className="flex mt-2 justify-end">
+            <IconButton onClick={handleDrawerClose}>{theme.direction === "rtl" ? <ChevronLeftIcon /> : <ChevronRightIcon />}</IconButton>
+          </div>
+          <List className="flex flex-col gap-1 justify-center">
+            {options.map((text, index) => (
+              <ListItem key={index} disablePadding>
+                <Link className="w-full" to={text.link} onClick={handleDrawerClose}>
+                  <ListItemButton>
+                    <ListItemText className="text-black" primary={text.name} />
+                  </ListItemButton>
+                </Link>
+              </ListItem>
+            ))}
+            <ListItemButton onClick={logout}>
+              <ListItemText className=" text-red-500" primary="Log Out" />
+            </ListItemButton>
+          </List>
+        </Drawer>
+      </Hidden>
+    </div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,24 +8,27 @@ import HomeCard from "../components/HomeCard";
 
 const features = [
   {
-    imageUrl: "https://cdn.discordapp.com/attachments/1184992990868033647/1225657863993557012/jayblesdrums_High_quality_photography_of_a_street_art_fair_boot_ddeec5d1-8855-4a92-b3f3-06c7293f17d7.png?ex=6621edd6&is=660f78d6&hm=caadb9378e8b416777ea8e9a656c3ffe3a9dbf51fb69e63e6ece258a68ad94d7&",
+    imageUrl:
+      "https://cdn.discordapp.com/attachments/1184992990868033647/1225657863993557012/jayblesdrums_High_quality_photography_of_a_street_art_fair_boot_ddeec5d1-8855-4a92-b3f3-06c7293f17d7.png?ex=6621edd6&is=660f78d6&hm=caadb9378e8b416777ea8e9a656c3ffe3a9dbf51fb69e63e6ece258a68ad94d7&",
     title: "Browse Our Products",
     splash: "New products featured weekly",
-    link: "/shop"
+    link: "/shop",
   },
   {
-    imageUrl: "https://cdn.discordapp.com/attachments/1184992990868033647/1227018621482700902/jayblesdrums_Happy_Artists_b4943c78-8b0f-4363-a1b2-3fb64e4c3f13.png?ex=6626e124&is=66146c24&hm=3814b80171b699345dd31674ca0e480248baee8923524eaa7fca7a0882ba63e6&",
+    imageUrl:
+      "https://cdn.discordapp.com/attachments/1184992990868033647/1227018621482700902/jayblesdrums_Happy_Artists_b4943c78-8b0f-4363-a1b2-3fb64e4c3f13.png?ex=6626e124&is=66146c24&hm=3814b80171b699345dd31674ca0e480248baee8923524eaa7fca7a0882ba63e6&",
     title: "Learn About The Fair",
     splash: "Who we are",
-    link: "/about"
+    link: "/about",
   },
   {
-    imageUrl: "https://cdn.discordapp.com/attachments/1184992990868033647/1227019176623865896/jayblesdrums_Holding_Paintbrush_dba8aea8-001c-4359-887c-ab2a90bede3f.png?ex=6626e1a8&is=66146ca8&hm=46056cb556539f158bc5204cc0b3fc64ac6f2535d2c07543d1f148d4691195bd&",
+    imageUrl:
+      "https://cdn.discordapp.com/attachments/1184992990868033647/1227019176623865896/jayblesdrums_Holding_Paintbrush_dba8aea8-001c-4359-887c-ab2a90bede3f.png?ex=6626e1a8&is=66146ca8&hm=46056cb556539f158bc5204cc0b3fc64ac6f2535d2c07543d1f148d4691195bd&",
     title: "Meet The Artists!",
-    splash: "Save your Favorites!", 
-    link: "/artists"
-  }
-]
+    splash: "Save your Favorites!",
+    link: "/artists",
+  },
+];
 
 const Home = () => {
   return (
@@ -33,14 +36,14 @@ const Home = () => {
       <SplashBannerLg />
       <div className="w-screen flex justify-center mb-2">
         <div className="flex w-4/5 justify-center p-5 items-center border-black bg-transparent text-black">
-          <h2>Hand made directly directly from the artists to you!</h2>
+          <h2>Hand made directly from the artists to you!</h2>
         </div>
       </div>
       <div className="w-screen flex justify-center mb-2">
         <div className="flex w-4/5 justify-center">
           {features.map((feature, index) => (
             <Link key={index} to={feature.link}>
-            <HomeCard key={index} feature={feature} />
+              <HomeCard key={index} feature={feature} />
             </Link>
           ))}
         </div>
